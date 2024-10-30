446175a76db75165f9c6294c5589a494ae5adf2b
Bazinga3 <b.barbieri7@studenti.unipi.it>
2024-10-30 16:50:42 +0100

    small latex setup pt2

--- code/main.py
+++ code/main.py
@@ -18,23 +18,3 @@ for col in num_cols:
 train_df.loc[train_df['genres'] == r'\N', 'genres'] = np.nan
 
 # train_df
-
-
-complete_df = pd.concat([train_df, test_df], ignore_index = True)
-
-
-import matplotlib.pyplot as plt
-
-
-plt.figure(figsize = (10, 6))
-
-
-grouped_ratings = complete_df.groupby('rating').count()
-grouped_ratings['originalTitle'].plot(kind= 'bar')
-
-# to save: plt.savefig('plots/<plot_name>.png')
-plt.savefig('plots/rating_count.png')
-
-
-# avg awardWins per rating
-avg_awardWins = complete_df.groupby('rating')['awardWins'].mean().reset_index()
