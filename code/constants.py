14b9fb561e67ed3d4c798eeb93250bc6b908a39f
noemidalmasso <162735552+noemidalmasso@users.noreply.github.com>
2024-11-04 10:19:08 +0100

    Update constants.py with merged df

--- code/constants.py
+++ code/constants.py
@@ -6,3 +6,11 @@ numeric_columns = [
         'criticReviewsTotal', 'awardNominationsExcludeWins',
         'numRegions', 'userReviewsTotal', 'ratingCount'
     ]
+
+import pandas as pd
+train_df = pd.read_csv("dm1_dataset_2425_imdb/train.csv")
+test_df = pd.read_csv("dm1_dataset_2425_imdb/test.csv")
+
+complete_df = pd.concat([train_df, test_df], ignore_index = True)
+
+complete_df.to_csv("complete_df.csv", index=False)
