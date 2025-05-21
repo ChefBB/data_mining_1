a27ce8d2de53b92897cd8af0678c510b7d0966df
Bazinga3 <b.barbieri7@studenti.unipi.it>
2025-05-21 15:53:47 +0200

    final outliers

--- code/support/outliers.py
+++ code/support/outliers.py
@@ -14,6 +14,64 @@ power_law_columns = [
 ]
 
 
+def detect_outliers_runtime_no_title_type(train, test, lower_percentile=1, upper_percentile=99):
+    """
+    Detects outliers in the 'runtimeMinutes_notitletype' column of train and test datasets.
+
+    Parameters:
+        train (array-like): Training feature values.
+        test (array-like): Testing feature values.
+        lower_percentile (float | None): Lower percentile threshold (default 1). Assumes None if feature is Power Law-Like.
+        upper_percentile (float): Upper percentile threshold (default 99).
+
+    Returns:
+        train (pd.DataFrame): Training dataset with outliers marked and imputed.
+        test (pd.DataFrame): Testing dataset with outliers marked and imputed.
+    """
+    train['outlier_no_type'] = False
+    test['outlier_no_type'] = False
+    column = 'runtimeMinutes_notitletype'
+    train_outlier_mask, lower_thresh, upper_thresh = detect_outliers_by_percentile(
+        train[column], lower_percentile=0 if power_law_columns.__contains__(column) else lower_percentile,
+        upper_percentile=upper_percentile)
+    test_outlier_mask = np.logical_or(test[column] < lower_thresh, test[column] > upper_thresh)
+    print(column, lower_thresh, upper_thresh)
+    train[column + '_imputed_no_type'] = impute_outliers(train, lower_thresh, upper_thresh, feature=column)[column]
+    test[column + '_imputed_no_type'] = impute_outliers(test, lower_thresh, upper_thresh, feature=column)[column]
+    train['outlier_no_type'] = np.logical_or(train['outlier_no_type'], train_outlier_mask)
+    test['outlier_no_type'] = np.logical_or(test['outlier_no_type'], test_outlier_mask)
+    return train, test
+
+
+def detect_outliers_runtime(train, test, lower_percentile=1, upper_percentile=99):
+    """
+    Detects outliers in the 'fill_runtimeMinutes_Bruno' column of train and test datasets.
+
+    Parameters:
+        train (array-like): Training feature values.
+        test (array-like): Testing feature values.
+        lower_percentile (float | None): Lower percentile threshold (default 1). Assumes None if feature is Power Law-Like.
+        upper_percentile (float): Upper percentile threshold (default 99).
+
+    Returns:
+        train (pd.DataFrame): Training dataset with outliers marked and imputed.
+        test (pd.DataFrame): Testing dataset with outliers marked and imputed.
+    """
+    train['outlier_w_type'] = False
+    test['outlier_w_type'] = False
+    column = 'fill_runtimeMinutes_Bruno'
+    train_outlier_mask, lower_thresh, upper_thresh = detect_outliers_by_percentile(
+        train[column], lower_percentile=0 if power_law_columns.__contains__(column) else lower_percentile,
+        upper_percentile=upper_percentile)
+    test_outlier_mask = np.logical_or(test[column] < lower_thresh, test[column] > upper_thresh)
+    print(column, lower_thresh, upper_thresh)
+    train[column + '_imputed_w_type'] = impute_outliers(train, lower_thresh, upper_thresh, feature=column)[column]
+    test[column + '_imputed_w_type'] = impute_outliers(test, lower_thresh, upper_thresh, feature=column)[column]
+    train['outlier_w_type'] = np.logical_or(train['outlier_w_type'], train_outlier_mask)
+    test['outlier_w_type'] = np.logical_or(test['outlier_w_type'], test_outlier_mask)
+    return train, test
+
+
 def detect_outliers_by_percentile(data, lower_percentile=1, upper_percentile=99):
     """
     Detects outliers in a 1D array based on percentile thresholds.
